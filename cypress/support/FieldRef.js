f6e2a926165441188e7918cd4d7ad0d99c051bd3
Dhananjay Lokhande <dlokhande4836552@gmail.com>
2024-10-20 21:01:22 +0200

    End to end code for shopping flow

---
+++ cypress/support/FieldRef.js
@@ -0,0 +1,32 @@
+//login
+export const USERID_INPUT = 'input[name="user-name"]';
+export const PASSWORD_INPUT = 'input[name="password"]';
+export const LOGIN_BUTTON = 'input[type="submit"]';
+
+//Shopping cart
+export const INVETORY_ITEM = '[data-test="item-4-title-link"] > [data-test="inventory-item-name"]';
+export const PRODUCT_DESC = '[data-test="inventory-item-desc"]';
+export const ADD_TO_CART = '[data-test="add-to-cart"]';
+export const ADD_TO_CART_LINK = '[data-test="shopping-cart-link"]';
+export const CHECKOUT = '[data-test="checkout"]';
+
+//Person information
+export const FIRST_NAME = '[data-test="firstName"]';
+export const LAST_NAME = '[data-test="lastName"]';
+export const PIN_CODE = '[data-test="postalCode"]';
+
+//Continue Page
+export const CONTINUE = '[data-test="continue"]';
+
+//Finish 
+export const FINISH = '[data-test="finish"]';
+
+//Last page comment
+export const THANK_YOU = '[data-test="complete-header"]';
+
+//invalid login
+// Error message for wrong password
+export const ERROR_MSG_INVALID_INPUT = '.error-message-container';
+
+//Error message on empty filed
+export const ERROR_MSG_EMPTY_INPUT = '[data-test="error"]';
